f307dcd5d56e6f42112954aeed591c07dab3aef6
Andrew Jones <ajones@digitalbazaar.com>
2019-11-13 16:13:38 -0500

    use puppeteer to fill in payment information.

---
+++ test/helper.js
@@ -0,0 +1,93 @@
+const puppeteer = require('puppeteer');
+
+const setInputValue = async ({selector, value, page}) => {
+  await page.focus(selector);
+  await page.keyboard.type(value, {delay: 200});
+};
+
+const selectors = {
+  form: 'form#singlePagePaymentForm',
+  accountBtn: 'a#createAccount',
+  card: {
+    number: 'input[name="cardNumber"]',
+    expires: 'input[name="expiry_value"]',
+    securityCode: 'input[name="cvv"]',
+    firstName: 'input#firstName',
+    lastName: 'input#lastName'
+  },
+  address: {
+    lineOne: 'input#billingLine1',
+    lineTwo: 'input#billingLine2',
+    city: 'input#billingCity',
+    state: 'select[name="billingState"]',
+    zipCode: 'input#billingPostalCode'
+  },
+  contact: {
+    phone: 'input#telephone',
+    email: 'input#email'
+  },
+  noSignUp: 'input#guestSignup2 + label',
+  continueBtn: 'button[type="submit"]'
+};
+
+const fillInCard = async ({card, order}) => {
+  const {href: approvalLink} = order.links.find(l => l.rel === 'approve');
+  console.log('approvalLink', approvalLink);
+  if(!approvalLink) {
+    throw new Error('approvalLink required');
+  }
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(approvalLink);
+  const creditCardButton = await page.$(selectors.accountBtn);
+  await creditCardButton.click();
+  await page.waitForSelector(selectors.form);
+  await setInputValue(
+    {selector: selectors.card.number, page, value: card.number});
+  await setInputValue(
+    {selector: selectors.card.expires, page, value: card.expiry});
+  await setInputValue(
+    {selector: selectors.card.securityCode, page, value: card.securityCode});
+  await setInputValue(
+    {selector: selectors.card.firstName, page, value: 'FirstName'});
+  await setInputValue(
+    {selector: selectors.card.lastName, page, value: 'LastName'});
+  await setInputValue(
+    {selector: selectors.address.lineOne, page, value: card.address.lineOne});
+  await setInputValue(
+    {selector: selectors.address.lineTwo, page, value: card.address.lineTwo});
+  await setInputValue(
+    {selector: selectors.address.city, page, value: card.address.adminAreaTwo});
+  await setInputValue({
+    selector: selectors.address.state,
+    page,
+    value: card.address.adminAreaOne
+  });
+  await setInputValue({
+    selector: selectors.address.zipCode,
+    page,
+    value: card.address.postalCode
+  });
+  await setInputValue(
+    {selector: selectors.contact.phone, page, value: '5409230456'});
+  await setInputValue({
+    selector: selectors.contact.email,
+    page,
+    value: 'tester@digitalbazaar.com'
+  });
+  await page.focus(selectors.noSignUp);
+  const noSignUp = await page.$(selectors.noSignUp);
+  await noSignUp.click();
+  const ctnBtn = await page.$(selectors.continueBtn);
+  await page.screenshot({path: 'approval_two.png'});
+  await ctnBtn.click();
+  await page.screenshot({path: 'approval_three.png'});
+  await page.waitForSelector(
+    selectors.continueBtn, {hidden: true, timeout: 45000});
+  await page.screenshot({path: 'approval_four.png'});
+  await browser.close();
+};
+
+module.exports = {
+  fillInCard
+};
--- test/mocha/040-process.js
+++ test/mocha/040-process.js
@@ -2,30 +2,38 @@
  * Copyright (c) 2018-2019 Digital Bazaar, Inc. All rights reserved.
  */
 'use strict';
-
+const bedrock = require('bedrock');
 const {api, test} = require('bedrock-payment-paypal');
 const {cards} = require('../cards');
+const {fillInCard} = require('../helper');
+
+const minute = 60000;
+const threeMinutes = 3 * minute;
 
 describe('processGatewayPayment', function() {
 
   it('should process a created payment', async function() {
-    const payment = {
+    const paymentData = {
+      id: `urn:uuid:${bedrock.util.uuid()}`,
       currency: 'USD',
       amount: '10.00',
       orderService: 'test-order',
       orderId: 'test-1'
     };
-    const {order} = await api.createGatewayPayment({payment});
+    const {order, payment} = await api.createGatewayPayment(
+      {payment: paymentData});
     should.exist(order);
     order.should.be.an('object');
     const card = cards.visa;
     try {
-      const paid = await test.capturePaymentOrder({order, card});
-      console.log('paid', paid);
+      await fillInCard({card, order});
+      await test.capturePaymentOrder({order});
+      const processed = await api.processGatewayPayment({payment});
+      console.log('processed', processed);
     } catch(e) {
-      console.error(e.response.data);
+      console.error(e);
     }
-  });
+  }).timeout(threeMinutes);
 
   it('should process an updated payment', async function() {
 
