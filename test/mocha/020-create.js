c7e948abee39e489b997cc9bfdf23bbbc32eea0f
Andrew Jones <ajones@digitalbazaar.com>
2019-11-14 16:40:58 -0500

    Change first tests over to nock.

--- test/mocha/020-create.js
+++ test/mocha/020-create.js
@@ -4,22 +4,40 @@
 'use strict';
 
 const {api} = require('bedrock-payment-paypal');
-const {util} = require('bedrock');
+const {util, config} = require('bedrock');
+const nock = require('nock');
 const {Errors} = require('bedrock-payment');
+const {mockPaypal} = require('../mock-paypal');
 
 const {BedrockError} = util;
 const minute = 60000;
 const twoMinutes = minute * 2;
+const {api: baseURL} = config.paypal;
+
+const createUrl = `/v2/checkout/orders`;
 
 describe('createGatewayPayment', function() {
+  beforeEach(function() {
+    if(!nock.isActive()) {
+      nock.activate();
+    }
+  });
+
+  afterEach(function() {
+    nock.restore();
+  });
 
   it('should create a payment.', async function() {
+    const paypalId = `urn:uuid:${util.uuid()}`;
     const payment = {
       currency: 'USD',
       amount: '10.00',
       orderService: 'test-order',
       orderId: 'test-1'
     };
+    const order = mockPaypal(
+      {id: paypalId, referenceId: payment.id, status: 'CREATED'});
+    nock(baseURL).post(createUrl).reply(200, order);
     const testResult = await api.createGatewayPayment({payment});
     should.exist(testResult);
     testResult.should.be.an('object');
